04d0e8bfc2348afffbd2e96292272155a8a70fa5
Lisa Chen <lisachen@littlelanterncreative.com>
2025-05-01 17:11:30 -0700

    task: create template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-        <h1 class="text-3xl font-bold underline">
+        <h1 class="bg-mint-500 text-3xl font-bold underline">
           Hello world!
         </h1>
     </>
