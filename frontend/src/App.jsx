fd6374dcc4b652abc3438bad2f65357246b9224c
Lisa Chen <lisachen@littlelanterncreative.com>
2025-05-10 14:11:11 -0700

    task: name and email components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
 
 function App() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleOnChange = (e) => {
+        e.preventDefault();
+        setName(e.target.value);
+    }
 
   return (
     <>
-        <h1 class="bg-mint-500 text-3xl font-bold underline">
+        <h1 class="font-script bg-mint-500 text-3xl font-bold underline">
           Hello world!
         </h1>
+        <form>
+            <label> Name
+                <input
+                    type="text"
+                    value={name}
+                    onChange={handleOnChange}
+                />
+            </label>
+            <label> Name
+                <input
+                    type="email"
+                    value={email}
+                    onChange={handleOnChange}
+                />
+            </label>
+            <input type="submit" />
+
+        </form>
     </>
   )
 }
