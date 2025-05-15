b21c32ed385dc4c0cfce431fa48bfbfef382bb61
Lisa Chen <lisachen@littlelanterncreative.com>
2025-05-14 22:33:37 -0700

    task: create partial jsx form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,43 +3,75 @@ import './App.css';
 
 
 function App() {
-    data = [
-        {"name":"Lisa"},
-        {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             }
-    }
     const [name, setName] = useState('');
+    const [phone, setPhone] = useState('')
     const [email, setEmail] = useState('');
+    const [guestCount, setGuestCount] = useState(1);
+    const [allergies, setAllergies] = useState('');
 
-    const handleOnChange = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        setName(e.target.value);
+        console.log("Submitted!");
+        console.log("Name: ", name);
+        console.log("Phone: ", phone)
+        console.log("Email: ", email);
+        console.log("guestCount: ", guestCount);
+        console.log("allergies: ", allergies);
     }
 
-  return (
-    <>
-        <h1 class="font-script bg-mint-500 text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <form>
-            <label> Name
-                <input
-                    type="text"
-                    value={name}
-                    onChange={handleOnChange}
-                />
-            </label>
-            <label> Name
-                <input
-                    type="email"
-                    value={email}
-                    onChange={handleOnChange}
-                />
-            </label>
-            <input type="submit" />
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">
+                    <input
+                        required
+                        id="name"
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Jane Doe"/>
+                </label>
+                <label htmlFor="phone">
+                    <input
+                        id="phone"
+                        type="tel"
+                        name="phone"
+                        value={phone}
+                        pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                        onChange={(e) => setPhone(e.target.value)}
+                        autoComplete="tel"
+                        placeholder="e.g., 123-123-1234" />
+                </label>
+                 <label htmlFor="email">
+                    <input
+                        id="email"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="iAmAUsername@email.com"/>
+                 </label>
+                 <label htmlFor="guestCount">
+                    <input
+                        id="guestCount"
+                        type="number"
+                        value={guestCount}
+                        onChange={(e) => setGuestCount(Number(e.target.value))} />
+                 </label>
+                 <label htmlFor="allergies">
+                    <textarea
+                        id="allergies"
+                        value={allergies}
+                        onChange={(e) => setAllergies(e.target.value)}
+                        cols="30"
+                        rows="5"
+                        placeholder="e.g. pineapple, gluten, dairy..."
+                        />
+                 </label>
 
-        </form>
-    </>
-  )
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default App
