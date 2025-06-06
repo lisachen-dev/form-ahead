994fe5e2703eedfcec2c03cc6a3be178b39838fe
Lisa <lisa.hl.chen@gmail.com>
2025-06-06 16:09:54 -0700

    task: refactor textarea element into it's own component

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -4,4 +4,5 @@ export { default as EmailInput } from './EmailInput';
 export { default as Form } from './Form';
 export { default as NumberInput } from './NumberInput';
 export { default as TextInput } from './TextInput';
+export { default as TextAreaInput } from './TextAreaInput';
 export { default as TelInput } from './TelInput';
