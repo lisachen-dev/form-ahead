6351aa12dd2576014662b711d2caaa76140fb56d
lisa-chen-58 <lisa.hl.chen@gmail.com>
2025-06-13 13:32:52 -0700

    task: spring boot base backend

---
+++ formapi/src/test/java/com/formahead/formapi/FormapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formahead.formapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
