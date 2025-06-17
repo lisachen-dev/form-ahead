84862ba519471966a9381947c51db97f54dab5c6
Lisa <lisa.hl.chen@gmail.com>
2025-06-16 19:11:26 -0700

    task: create form response object

--- formapi/src/main/java/com/formahead/formapi/model/FormResponse.java
+++ formapi/src/main/java/com/formahead/formapi/model/FormResponse.java
@@ -1,20 +1,36 @@
 package com.formahead.formapi.model;
 
-import lombok.Data;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ElementCollection;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+
 import java.util.List;
 import java.util.UUID;
 
-@Entity // TODO tells Spring Boot this class maps to a DB table
-@Data
+
+@Entity // Required for JPA
+@Data // Lombok: Generates getters, setters, toString, etc.
 public class FormResponse {
 
     @Id // TODO primary key of the current entity
     @GeneratedValue(strategy = GenerationType.UUID)
-    private Long id;
+    private UUID id;
 
     private String fullName;
     private String phone;
-    private
+
+    @Email
+    private String email;
+    private int guestCount;
+    private String allergies;
+    private String paymentMethod;
+
+    @ElementCollection
+    private List<String> availableDays;
+    private String additionalNotes;
 
 }
