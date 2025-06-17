cae62e345833a7a9643d174d992ec7669bb12866
lisa-chen-58 <lisa.hl.chen@gmail.com>
2025-06-16 22:37:18 -0700

    task: first passthrough for form response object

--- formapi/src/main/java/com/formahead/formapi/service/FormResponseService.java
+++ formapi/src/main/java/com/formahead/formapi/service/FormResponseService.java
@@ -1,1 +1,22 @@
 package com.formahead.formapi.service;
+
+import com.formahead.formahead.model.FormResponse;
+import com.formahead.formahead.repository.FormResponseRepository;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class FormResponseService {
+
+    private final FormResponseRepository formResponseRepository;
+
+    public List<FormResponse> getAllFormResponses() {
+        return formResponseRepository.findAll();
+    }
+
+    public FormResponse createFormResponse(FormResponse formResponse) {
+        return formResponseRepository.save(formResponse);
+    }
+}
